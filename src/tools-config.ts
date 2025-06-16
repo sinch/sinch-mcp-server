4675950a7b65f723c62caa5bf1891fecdcbb17ed
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2025-06-16 15:35:54 +0200

    Fix comment 2

--- src/tools-config.ts
+++ src/tools-config.ts
@@ -1,2 +1,9 @@
+/**
+ * Map of the tools' status: the key is the tool's name, and the value is its status.
+ * If the tool is not filtered out and its credentials are defined, its description should be ENABLED.
+ * If the tool is filtered out or its credentials are not defined, its description should be a reason why it is disabled.
+ */
 export const toolsStatusMap: Record<string, string> = {};
+
+/** The tool is not filtered out and its credentials are defined => its description should be ENABLED */
 export const ENABLED = 'enabled';
